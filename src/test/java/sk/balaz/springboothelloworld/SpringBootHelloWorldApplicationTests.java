2e37db3303e977e77081e09cd270a23fd451abff
Tomas Balaz <tomasba@amdocs.com>
2022-03-23 15:30:39 +0100

    first commit

---
+++ src/test/java/sk/balaz/springboothelloworld/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
